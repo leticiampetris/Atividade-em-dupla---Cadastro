c020945f286c8178a8175e47a4bd4cf5db499637
leticiampetris <leticiampetris@gmail.com>
2018-05-22 10:06:41 -0300

    Alterações em Delegacias

--- Cadastro Polícia/src/CadastroDelegacias.java
+++ Cadastro Polícia/src/CadastroDelegacias.java
@@ -1,13 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Alunos
+ * @author Leticia
  */
 public class CadastroDelegacias {
-    
+
+    static String[] nomes = new String[100];
+    String[] enderecos = new String[100];
+    String[] telefones = new String[100];
+    String[] horarios = new String[100];
+    String[] delegados = new String[100];
+    String[] datas = new String[100];
+    String[] areas = new String[100];
+    static int atual = 0;
+
+    public void solicitarInformacao(int posicao) {
+
+        nomes[posicao] = JOptionPane.showInputDialog("Digite o nome da delegacia:");
+        enderecos[posicao] = JOptionPane.showInputDialog("Digite o endereço:");
+        telefones[posicao] = JOptionPane.showInputDialog("Digite o telefone:");
+        horarios[posicao] = JOptionPane.showInputDialog("Digite o horário de funcionamento:");
+        delegados[posicao] = JOptionPane.showInputDialog("Digite o nome do delegado:");
+        datas[posicao] = JOptionPane.showInputDialog("Digite a data de início do funcionamento:");
+        areas[posicao] = JOptionPane.showInputDialog("Informe a área de abrangência:");
+    }
+
+    public void cadastrar() {
+        solicitarInformacao(atual);
+        atual++;
+    }
+
+    public void editar() {
+
+        String busca = JOptionPane.showInputDialog("Digite parte do nome da delegacia para editar:")
+                .trim().replace(".", "");
+        for (int posicao = 0; posicao < atual; posicao++) {
+            /* if (placas[posicao].equals(busca)) {
+             solicitarInformacao(posicao);
+             return;
+             }*/
+
+        }
+    }
+
+    public void listar() {
+
+        String texto = "";
+        for (int i = 0; i < atual; i++) {
+//            texto += placas[i] + "   " + tipos[i] + "\n";
+        }
+        JOptionPane.showMessageDialog(null, texto);
+
+    }
+
 }
