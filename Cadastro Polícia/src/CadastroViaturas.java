e34796dbbfd0e4b879aece29bd90385234fac7a7
leticiampetris <leticiampetris@gmail.com>
2018-05-22 09:17:07 -0300

    Continuação classes Viaturas e Delegacias

--- Cadastro Polícia/src/CadastroViaturas.java
+++ Cadastro Polícia/src/CadastroViaturas.java
@@ -1,6 +1,4 @@
 
-package cadastro.polícia;
-
 import javax.swing.JOptionPane;
 
 /**
@@ -9,41 +7,39 @@ import javax.swing.JOptionPane;
  */
 public class CadastroViaturas {
 
-   
     String[] tipos = new String[100];
     String[] marcas = new String[100];
     String[] modelos = new String[100];
     int[] anos = new int[100];
     String[] placas = new String[100];
     String[] licenciamentos = new String[100];
-    int[] delegacias = new int[100];
+    String[] delegacias = new String[100];
     int atual = 0;
-      
+
     public void solicitarInformacao(int posicao) {
 
         tipos[posicao] = JOptionPane.showInputDialog(null,
-        "Selecione o tipo de viatura:",
-        "VIATURAS",
-        JOptionPane.QUESTION_MESSAGE,
-        null,
-        new Object []{
-            "Automóvel", "Motocicleta"}, null).toString();
+                "Selecione o tipo de viatura:",
+                "VIATURAS",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{
+                    "Automóvel", "Motocicleta"}, null).toString();
         marcas[posicao] = JOptionPane.showInputDialog("Digite a marca:");
         modelos[posicao] = JOptionPane.showInputDialog("Digite o modelo:");
         anos[posicao] = Integer.parseInt(JOptionPane.showInputDialog("Digite o ano:"));
         placas[posicao] = JOptionPane.showInputDialog("Digite a placa:");
         licenciamentos[posicao] = JOptionPane.showInputDialog("Digite a data do próximo licenciamento:");
         modelos[posicao] = JOptionPane.showInputDialog("Digite o modelo:");
-        delegacias[posicao] = Integer.parseInt(JOptionPane.showInputDialog(null,
-        "Selecione a delegacia de vinculação:",
-        "VIATURAS",
-        JOptionPane.QUESTION_MESSAGE,
-        null,
-        new Object []{
-            "DRP Centro", "DRP Norte", "DRP Sul"}, null).toString());
-        }
-        
-    public void cadastrar(){
+        delegacias[posicao] = JOptionPane.showInputDialog(null,
+                "Selecione a delegacia de vinculação:",
+                "VIATURAS",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{"DRP Centro", "DRP Norte", "DRP Sul"}, null).toString();
+    }
+
+    public void cadastrar() {
         solicitarInformacao(atual);
         atual++;
     }
@@ -51,17 +47,16 @@ public class CadastroViaturas {
     public void editar() {
 
         String busca = JOptionPane.showInputDialog("Digite a placa para editar:")
-                       .trim().toUpperCase().replace("-", "");
-        for (int i = 0; i < atual; i++) {
-            if (placas[i].equals(busca)) {
-                solicitarInformacao(i);
+                .trim().toUpperCase().replace("-", "");
+        for (int posicao = 0; posicao < atual; posicao++) {
+            if (placas[posicao].equals(busca)) {
+                solicitarInformacao(posicao);
                 return;
             }
 
         }
     }
-    
-    
+
     public void listar() {
 
         String texto = "";
@@ -70,5 +65,5 @@ public class CadastroViaturas {
         }
         JOptionPane.showMessageDialog(null, texto);
     }
-      
+
 }
---
+++ Cadastro Polícia/src/Viaturas.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Leticia
+ */
+public class Viaturas {
+
+    public void menuViatura() {
+        CadastroViaturas registroViaturas = new CadastroViaturas();
+        
+        
+        int menuViaturas = JOptionPane.showOptionDialog(null,
+                "Selecione a op��o desejada:", "VIATURAS", 0,
+                JOptionPane.QUESTION_MESSAGE, null,
+                new Object[]{
+                    "Cadastrar",
+                    "Editar",
+                    "Listar"}, null);
+
+        while (menuViaturas < 3) {
+            switch (menuViaturas) {
+                case 0:
+                    registroViaturas.cadastrar();
+                    break;
+                case 1:
+                    registroViaturas.editar();
+                    break;
+                case 2:
+                    registroViaturas.listar();
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Sistema encerrado.");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new Viaturas().menuViatura();
+    }
+}
