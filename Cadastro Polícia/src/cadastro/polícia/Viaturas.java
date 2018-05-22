b9441f1ca459da65fa659df229ac655a04d8c908
leticiampetris <leticiampetris@gmail.com>
2018-05-22 08:32:28 -0300

    Alterações em Viaturas

--- Cadastro Polícia/src/cadastro/polícia/Viaturas.java
+++ Cadastro Polícia/src/cadastro/polícia/Viaturas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cadastro.pol√≠cia;
 
 import javax.swing.JOptionPane;
@@ -15,9 +10,8 @@ public class Viaturas {
     
     CadastroViaturas registroViaturas = new CadastroViaturas();
 
-
     int menuViaturas = JOptionPane.showOptionDialog(null,
-                "Selecione a op√ß√£o desejada:", "VIATURAS", 0,
+                "Selecione a opÁ„o desejada:", "VIATURAS", 0,
                 JOptionPane.QUESTION_MESSAGE, null,
                 new Object[]{
                 "Cadastrar",
@@ -28,9 +22,8 @@ public class Viaturas {
     while (menuViaturas < 3) {
             switch (menuViaturas) {
                 case 0: registroViaturas.cadastrar();break;                  
-                case 1: break;                    
-                case 2: break;                    
-                case 3: break;                    
+                case 1: registroViaturas.editar();break;                 
+                case 2: registroViaturas.listar();break;                
                 default:
                     JOptionPane.showMessageDialog(null, "Sistema encerrado.");
                 }
