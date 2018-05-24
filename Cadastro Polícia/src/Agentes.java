fb833b54761806a0a7571442b0e59c9984af9b55
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2018-05-24 08:04:53 -0300

    Menu Agentes

--- Cadastro Polícia/src/Agentes.java
+++ Cadastro Polícia/src/Agentes.java
@@ -1,11 +1,54 @@
+
+import javax.swing.JOptionPane;
+
 public class Agentes {
     
     CadastroAgentes cadastroAgentes = new CadastroAgentes();
     
     public void menuAgentes(){
+        int opcao = JOptionPane.showOptionDialog(null, 
+                "Selecione a opção desejada", "AGENTES", 
+                0,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{
+                    "Cadastrar", "Editar", "Listar"
+                },null            
+        );
+        
+        while(opcao < 3 && opcao!= JOptionPane.CLOSED_OPTION){
+            
+            switch(opcao){
+                case 0: cadastroAgentes.cadastrarAgente();
+                    break;
+                case 1: cadastroAgentes.editarAgente();
+                    break;
+                case 2: cadastroAgentes.listarAgentes();
+                    break;
+                default: 
+                    JOptionPane.showMessageDialog(null, "Sistema Ecerrado");
+            }
+            
+            opcao = JOptionPane.showOptionDialog(null, 
+                "Selecione a opção desejada", "AGENTES", 
+                0,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{
+                    "Cadastrar", "Editar", "Listar"
+                },null            
+        );
+            
+        }
         
     }
     
+    public static void main(String[] args){
+        new Agentes(). menuAgentes();
+        
+        
+        
+    }
     
 
 }
